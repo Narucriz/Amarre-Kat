bc23abc97d9cb802fb375e362bd94042304fbce2
BuildTools <unconfigured@null.spigotmc.org>
2023-02-02 21:26:00 -0500

    Changes to be committed: 	modified:   antesdebody.js 	modified:   index.html

--- antesdebody.js
+++ antesdebody.js
@@ -5,7 +5,7 @@ let encabezados = "";
 window.onclick = function() {
     //Crea una serie de elements por cada tag encontrado
     let encabezados = document.createElement("h1")
-    const text = document.createTextNode("¿quieres ser mi Wafle?")
+    const text = document.createTextNode("¿Quieres ser mi Mejor Coincidencia?")
     
     encabezados.appendChild(text)
     document.body.appendChild(encabezados);
